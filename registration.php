bab4038b4aeeecfa3e386aa82dc144d0f3b4030d
MiyuniTharushika <90705980+MiyuniTharushika@users.noreply.github.com>
2023-07-10 15:58:41 +0530

    registration

---
+++ registration.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html
+  data-bs-theme="light"
+  lang="en"
+  style="margin-top: -101px; padding-top: 21px"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <title>registrationForm</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.12.0/css/all.css"
+    />
+    <link rel="stylesheet" href="assets/css/styles.min.css" />
+  </head>
+  <body style="background: url('assets/img/5.jpg'); background-size: cover">
+    <section
+      class="clean-block clean-form dark h-100"
+      style="padding-top: 0px; background: transparent"
+    >
+      <div class="container">
+        <div class="block-heading-1">
+          <h2 class="fs-1 text-light h2-reg">Registration Form</h2>
+          <p></p>
+        </div>
+        <form
+          action="index.html"
+          method="post"
+          enctype="multipart/form-data"
+          role="form"
+          class="form-bg"
+        >
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light">Full Name</label
+            ><input
+              class="form-control"
+              type="tel"
+              placeholder="Full Name"
+              name="phone"
+              required=""
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light">Degree Programme</label
+            ><input
+              class="form-control"
+              type="tel"
+              placeholder="Degree Programme"
+              name="phone"
+              required=""
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light"
+              >Enrollment Number</label
+            ><input
+              class="form-control"
+              type="tel"
+              placeholder="Enrollement Number"
+              name="phone"
+              required=""
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light">Academic Year</label
+            ><input
+              class="form-control"
+              type="tel"
+              placeholder="Academic Year"
+              name="phone"
+              required=""
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light">Phone No</label
+            ><input
+              class="form-control"
+              type="tel"
+              placeholder="Phone No"
+              name="phone"
+              required=""
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light">Email</label
+            ><input
+              class="form-control"
+              type="tel"
+              placeholder="Email"
+              name="phone"
+              required=""
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label class="form-label fw-bold text-light">Password</label
+            ><input
+              class="form-control"
+              type="password"
+              id="formum2"
+              placeholder="Password"
+            />
+          </div>
+          <label class="form-label fw-bold text-light label-gender"
+            >Gender</label
+          ><button class="btn btn-dark btn-sm" type="button">Male</button>
+          <div class="btn-group" role="group"></div>
+          <button class="btn btn-dark btn-sm" type="button">Female</button>
+          <hr class="hr-s" />
+          <div class="form-group mb-3">
+            <button
+              class="btn btn-primary d-block w-100 regi-button"
+              type="submit"
+            >
+              <i class="fas fa-save"></i>&nbsp;REGISTER
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://geodata.solutions/includes/countrystate.js"></script>
+  </body>
+</html>
